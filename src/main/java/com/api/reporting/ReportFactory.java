e2d18fd190832b45964fef6104666ed54b26d0a7
Swetha.BA <swetha.ba@24B-MACABL-0868.local>
2024-05-22 13:13:34 +0530

    Added Tag number

--- src/main/java/com/api/reporting/ReportFactory.java
+++ src/main/java/com/api/reporting/ReportFactory.java
@@ -63,6 +63,7 @@ public class ReportFactory {
 	private static String EnvironmentURL;
 	private static String ReportName;
 	private static String applicationName;
+	private static String tagNumber;
 	private static String ReportLink;
 	private static Integer totalTests;
 	private static Integer totalPassTests;
@@ -351,6 +352,10 @@ public class ReportFactory {
 	}
 	public static void PublishReportOnSlack() {
 		applicationName = System.getProperty("ApplicationName");
+		tagNumber = System.getProperty("tagNumber");
+		if(tagNumber.isEmpty() || tagNumber.equals("")){
+			tagNumber = "Scheduled Job";
+		}
 		for(String channel: ChannelID.split(",")) {
 			System.out.println(channel);
 			List<String> blocks = new ArrayList<String>();
@@ -360,11 +365,12 @@ public class ReportFactory {
 			String messageText="";
 			String messageTextPart="";
 			if(totalFailTests==0) {
-				messageText = "<!here>, *Application Name : "+ applicationName+"*,*"+ReportName+"*,>*Environment : <"+Environment+">*,>*Total Tests : "+totalTests+"*,>*Passed : "+totalPassTests+"*,>*Failed : "+totalFailTests+"*,>*Failed Tests :* _NA_,>*Test Report :*  _See Next Bot Message_";
+				messageText = "<!here>, *Application Name : "+ applicationName+"*,*"+ReportName+"*,>*Tag Number : <"+tagNumber+">*,>*Environment : <"+Environment+">*,>*Total Tests : "+totalTests+"*,>*Passed : "+totalPassTests+"*,>*Failed : "+totalFailTests+"*,>*Failed Tests :* _NA_,>*Test Report :*  _See Next Bot Message_";
 				message = "<!here>"
 						+ "\n *Application Name : "+ applicationName+"*"
 						+ "\n *"+ReportName+"*"
-						+ "\n>*Environment : <"+Environment+">*"
+						+ "\n>*Tag Number : <"+tagNumber+">*"
+						+ "\n>*Environment : "+Environment+"*"
 						+ "\n>*Total Tests : "+totalTests+"*"
 						+ "\n>*Passed : "+totalPassTests+"*"
 						+ "\n>*Failed : "+totalFailTests+"*"
@@ -374,11 +380,12 @@ public class ReportFactory {
 				message="";
 				messageTextPart="";
 			}else {
-				messageText = "<!here>, *"+ applicationName+"*,*"+ReportName+"*,>*Environment : <"+Environment+">*,>*Total Tests : "+totalTests+"*,>*Passed : "+totalPassTests+"*,>*Failed : "+totalFailTests+"*,>*Failed Tests :* _View Thread_,>*Test Report :*  _See Next Bot Message_";
+				messageText = "<!here>, *"+ applicationName+"*,*"+ReportName+"*,>*Tag Number : <"+tagNumber+">*,>*Environment : <"+Environment+">*,>*Total Tests : "+totalTests+"*,>*Passed : "+totalPassTests+"*,>*Failed : "+totalFailTests+"*,>*Failed Tests :* _View Thread_,>*Test Report :*  _See Next Bot Message_";
 				message = "<!here>"
 						+"\n *"+applicationName+"*"
 						+ "\n *"+ReportName+"*"
-						+ "\n>*Environment : <"+Environment+">*"
+						+ "\n>*Tag Number : <"+tagNumber+">*"
+						+ "\n>*Environment : "+Environment+"*"
 						+ "\n>*Total Tests : "+totalTests+"*"
 						+ "\n>*Passed : "+totalPassTests+"*"
 						+ "\n>*Failed : "+totalFailTests+"*"
