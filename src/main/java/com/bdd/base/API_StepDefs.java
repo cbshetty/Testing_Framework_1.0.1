c8decb0f1a744c1bd890e8136ff83cba34713859
swetha-ba-qa <swetha.ba@angelbroking.com>
2024-11-11 17:57:56 +0530

    Added code to publish message in thread

--- src/main/java/com/bdd/base/API_StepDefs.java
+++ src/main/java/com/bdd/base/API_StepDefs.java
@@ -350,11 +350,6 @@ public class API_StepDefs {
 		ReportFactory.PublishReportOnSlack2();
 	}
 
-	@Given("Report is published on slack thread")
-	public void report_is_published_on_slack_thread() {
-		ReportFactory.PublishReportOnSlackThread2();
-	}
-
 	@Given("Start Scenario {string}")
 	public void start_Scenario(String Scenario) {
 	    Parallel_BaseClass.SetScenarioStatus(Scenario, "STARTED");
@@ -373,7 +368,7 @@ public class API_StepDefs {
 	@Given("Wait until status of scenario {string} is {string}")
 	public void wait_until_status_of_scenario_is(String Scenario, String Status) throws InterruptedException {
 		int StatusMatched=0;
-	    for(int i=0;i<3;i++) {
+	    for(int i=0;i<300;i++) {
 	    	if(Parallel_BaseClass.GetScenarioStatus(Scenario).equals(Status)) {
 	    		StatusMatched=1;
 	    		ReportFactory.PassTest("INFO :: Status of Scenario"+Scenario+" is "+Status);
